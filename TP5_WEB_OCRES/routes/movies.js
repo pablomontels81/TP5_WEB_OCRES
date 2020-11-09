e681d606950bb8eb4d737e68a50380b85ae10014
pablomontels81 <pablo.montels@edu.ece.fr>
2020-11-09 12:21:12 +0100

    Mise en place requête CRUD

--- TP5_WEB_OCRES/routes/movies.js
+++ TP5_WEB_OCRES/routes/movies.js
@@ -1,9 +1,69 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* GET movies listing. */
+router.get('/', (req, res) => {
+  // Get List of user and return JSON
+  res.status(200).json({ movies });
+});
+
+/* GET one movie (by the id). */
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  // Find user in DB
+  const movie = _.find(movies, ["id", id]);
+  // Return user
+  res.status(200).json({
+    message: 'Movie found!',
+    movie 
+  });
+});
+
+/* PUT new movie. */
+router.put('/', (req, res) => {
+  // Get the data from request from request
+  const { movie } = req.body;
+  // Create new unique id
+  const id = _.uniqueId();
+  // Insert it in array (normaly with connect the data with the database)
+  movies.push({ movie, id });
+  // Return message
+  res.json({
+    message: `Just added ${id}`,
+    movie: { movie, id }
+  });
+});
+
+/* DELETE movie. */
+router.delete('/:id', (req, res) => {
+  // Get the :id of the user we want to delete from the params of the request
+  const { id } = req.params;
+
+  // Remove from "DB"
+  _.remove(movies, ["id", id]);
+
+  // Return message
+  res.json({
+    message: `Just removed ${id}`
+  });
+});
+
+/* UPDATE movie. */
+router.post('/:id', (req, res) => {
+  // Get the :id of the user we want to update from the params of the request
+  const { id } = req.params;
+  // Get the new data of the user we want to update from the body of the request
+  const { movie } = req.body;
+  // Find in DB
+  const userToUpdate = _.find(movies, ["id", id]);
+  // Update data with new data (js is by address)
+  userToUpdate.movie = movie;
+
+  // Return message
+  res.json({
+    message: `Just updated ${id} with ${movie}`
+  });
 });
 
 module.exports = router;
